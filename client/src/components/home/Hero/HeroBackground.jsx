30bfe80d5c2d38805ae482bf335034b455a8e333
Kathan Patel <kathanpatel099@gmail.com>
2026-08-20 17:32:33 +0530

    style: update iframe attributes in HeroBackground.jsx as requested

--- client/src/components/home/Hero/HeroBackground.jsx
+++ client/src/components/home/Hero/HeroBackground.jsx
@@ -8,13 +8,13 @@ const HeroBackground = ({ smoothMouseX, smoothMouseY, smoothCursorX, smoothCurso
       <div className="hero-background-image">
         <iframe 
           className="hero-video-bg"
-          src="https://www.youtube-nocookie.com/embed/YI-mwsBshHE?autoplay=1&mute=1&loop=1&playlist=YI-mwsBshHE&controls=0&modestbranding=1&rel=0&iv_load_policy=3&disablekb=1&playsinline=1" 
-          title="YouTube video player" 
+          src="https://www.youtube.com/embed/YI-mwsBshHE?autoplay=1&mute=1&controls=0&loop=1&playlist=YI-mwsBshHE&rel=0&playsinline=1" 
+          title="Video" 
           frameBorder="0" 
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          allow="autoplay; encrypted-media" 
           tabIndex="-1"
           aria-hidden="true"
-          style={{ pointerEvents: 'none', userSelect: 'none' }}
+          style={{ border: "none", pointerEvents: 'none', userSelect: 'none' }}
         ></iframe>
         <img src={abrasiveSheet} alt="Industrial Background" className="hero-fallback-img" />
         <div className="hero-image-overlay" style={{ pointerEvents: 'auto' }} />

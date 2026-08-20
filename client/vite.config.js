ab2cdf9813811cc6159d01533811af928d2207d3
Krishna Panchal <krishnapanchal822006@gmail.com>
2026-08-20 18:55:27 +0530

    init

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: __dirname,
   plugins: [react()],
+  server: {
+    fs: {
+      strict: true,
+      allow: [__dirname],
+    },
+    watch: {
+      ignored: [
+        '**/$Recycle.Bin/**',
+        '**/OneDriveTemp/**',
+        '**/.git/**',
+        '**/node_modules/**',
+      ],
+    },
+  },
 })
